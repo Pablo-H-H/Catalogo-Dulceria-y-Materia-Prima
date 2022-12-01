3e2cff8b2c96f8640b82aecd2a42c7cdd0399e7a
Pablo H <111140061+Pablo-H-H@users.noreply.github.com>
2022-11-30 22:04:31 -0600

    Se quitaron los atributos innecesarios

--- Product.h
+++ Product.h
@@ -1,5 +1,9 @@
+#include <iostream>
 #ifndef PRODUCTO
 #define PRODUCTO
+#include <sstream>
+
+using namespace std;
 
 class Product
 {
@@ -7,88 +11,36 @@ class Product
         string nombre;
         string marca;
         float precio;
-        bool disponible;
-        bool entregaDomicilio;
     public:
-        Product(string, string, float, bool, bool);
-        string getNombre();
+        Product();
+        Product(string, string, float);
+        void getNombre();
+        string soloNombre();
     protected:
 
 
 };
 
-class ArticuloFiesta : public Product{
-    private:
-        int unidad;
-        string color;
-        float tamanio;
-    public:
-        ArticuloFiesta(string, string, float, bool, bool, int, string, float);
-        string getColor();
-};
 
-class Frituras : public Product {
-    private:
-        string sabor;
-        string tipo;
-        string forma;
-        float gramaje;
-    public:
-        Frituras(string, string, float, bool, bool, string, string, string, float);
-        string getSabor();
-};
-
-class Chocolate : public Product {
-    private:
-        string presentacion;
-        float cacaho;
-        float gramaje;
-    public:
-        Chocolate(string, string, float, bool, bool, string, float, float);
-        string getPresentacion();
-};
-
-Product::Product(string _nombre, string _marca, float _precio, bool _disponible, bool _entregaDomicilio){
+Product::Product(){}
+Product::Product(string _nombre, string _marca, float _precio){
     nombre = _nombre;
     marca = _marca;
     precio = _precio;
-    disponible = _disponible;
-    entregaDomicilio = _entregaDomicilio;
 }
 
-ArticuloFiesta::ArticuloFiesta(string _nombre, string _marca, float _precio, bool _disponible, bool _entregaDomicilio, int _unidad, string _color, float _tamanio) : Product(_nombre, _marca, _precio, _disponible, _entregaDomicilio){
-    unidad = _unidad;
-    color = _color;
-    tamanio = _tamanio;
+
+
+
+void Product::getNombre(){
+    cout << "Nombre: " << nombre << endl;
+    cout << "Marca: " << marca << endl;
+    cout << "Precio: " << precio << endl;
 }
 
-Frituras::Frituras(string _nombre, string _marca, float _precio, bool _disponible, bool _entregaDomicilio, string _sabor, string _tipo, string _forma, float _gramaje) : Product(_nombre, _marca, _precio, _disponible, _entregaDomicilio){
-    sabor = _sabor;
-    tipo = _tipo;
-    forma = _forma;
-    gramaje = _gramaje;
-}
 
-Chocolate::Chocolate(string _nombre, string _marca, float _precio, bool _disponible, bool _entregaDomicilio, string _presentacion, float _cacaho, float _gramaje) : Product(_nombre, _marca, _precio, _disponible, _entregaDomicilio){
-    presentacion = _presentacion;
-    cacaho = _cacaho;
-    gramaje = _gramaje;
-}
-
-string Product::getNombre(){
+string Product::soloNombre(){
     return nombre;
-}
-
-string ArticuloFiesta::getColor(){
-    return color;
-}
-
-string Frituras::getSabor(){
-    return sabor;
-}
-
-string Chocolate::getPresentacion(){
-    return presentacion;
 }
 
 #endif
